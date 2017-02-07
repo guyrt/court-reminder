fc3ea0ece5b87a221de8828508676c1f5fecf0cc
Tommy Guy <richardtguy84@gmail.com>
2017-02-06 19:55:43 -0800

    Moved code from first hack night into calls folder. Sample now calls, records, waits for 90 seconds, then retrieves the recording.

---
+++ call/place_call.py
@@ -0,0 +1,52 @@
+"""
+Place a Twilio phone call and record the outcome.
+"""
+
+from .secrets import *
+
+class TwilioCallWrapper(object):
+
+    def __init__(self, call_placed_callback=None, call_done_callback=None):
+        self.call_placed_callback = call_placed_callback
+        self.call_done_callback = call_done_callback
+
+        self._client = TwilioRestClient(twilio_account_sid, twilio_auth_token)
+        # twilio hits this to get a record of what to do.
+        self._callback_url = 'http://13.68.220.163/record.xml'
+
+    def build_dtmf_sequence(self, case_number):
+        """ Sequence represents the following:
+
+        Send 1  [to enter in English?]
+        Wait 1 Second
+        Send case number
+        Wait 1 Second
+        Send 1  [why?]
+        Wait 1 Second
+        Send 1  [why?]
+        Wait 1 Second
+        Send 1  [why?]
+        Wait for 10 seconds
+        Send 1  [trick into a repeat so we catch the full message.]
+        """
+        return "1ww{case_number}ww1ww1ww1".format(case_number=case_number) + ("w" * 5 * 2) + "1"
+
+    def place_call(self, case_number):
+        send_digits = self.build_dtmf_sequence(case_number)
+        call = self.client.calls.create(to=to_phone, from_=from_phone, url=self._callback_url, record=True, send_digits=send_digits)
+        if self.call_placed_callback:
+            self.call_placed_callback(call.sid)
+        self._handle_call(call_sid)
+
+    def _handle_call(self, call_sid):
+        time.sleep(90)  # sleep for a bit to let the call happen
+        call = self.client.calls.get(sid)
+        call.hangup()
+
+        # get a fresh call.
+        call = self.client.calls.get(sid)
+        recording = call.recordings.list()[0]
+        recording_uri = recording.uri
+
+        if self.call_done_callback:
+            self.call_done_callback(recording_uri)
---
+++ call/secrets.sample.py
@@ -0,0 +1,8 @@
+# These are your Twilio credentials
+twilio_account_sid = "<get this from Twilio>"
+twilio_auth_token = "<get this from Twilio>"
+
+# to_phone is the court number
+to_phone = '+18008675309'
+# from_phone is a number you have registered with Twilio
+from_phone = '+18008675309'
