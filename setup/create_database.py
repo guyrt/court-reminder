ec99fbdc3c65d1a7229925cc2ef99d04a9663e9f
Aditya <imadityab@gmail.com>
2017-02-16 21:56:33 -0800

    (untested) added Call Duration column and made update timestamp auto-updatable

--- setup/create_database.py
+++ setup/create_database.py
@@ -1,27 +1,43 @@
 import pyodbc
 
-from storage.secrets import db_connection_string, db_tablename
+from storage.secrets import db_connection_string, db_name
 
 conn = pyodbc.connect(db_connection_string,
                       autocommit=True) 
 cursor = conn.cursor()
 sqlcommand = """
-CREATE TABLE {db_tablename}
+BEGIN
+
+CREATE TABLE {db_name}
 (
     ID int NOT NULL IDENTITY,
     AlienRegistrationNumber varchar(255) NOT NULL UNIQUE,
     Status varchar(255),
     CallID varchar(255),
     CallTimestamp datetime,
+    CallDurationInSeconds int CHECK (CallDurationInSeconds >= 0),
     CallTranscript varchar(max),
     CallUploadUrl varchar(2100),
     CourtHearingType varchar(124),
     CourtHearingDate varchar(512),
     CourtHearingLocation varchar(1024),
     CourtHearingJudgeName varchar(1024),
-    LastUpdatedTimestamp datetime,
+    LastUpdatedTimestamp datetime DEFAULT(getdate()),
     PRIMARY KEY (ID)
 )
+
+CREATE TRIGGER dbo.autoUpdateTimestamp 
+ON dbo.{db_tablename}
+FOR UPDATE 
+AS 
+BEGIN 
+    IF NOT UPDATE(LastUpdatedTimestamp) 
+        UPDATE dbo.{db_tablename} SET LastUpdatedTimestamp=GETDATE() 
+        WHERE col1 IN (SELECT col1 FROM inserted) 
+END 
+GO
+
+END
              """.format(db_tablename=db_tablename)
 
 cursor.execute(sqlcommand)
