b95ffc773498118127362ce91ad1f898467aaaaa
Ashkan Aazami <aazami@gmail.com>
2017-02-26 20:05:54 -0800

    commit

--- transcribe/transcribe.py
+++ transcribe/transcribe.py
@@ -33,35 +33,3 @@ class BingTranscribe(object):
 foo = BingTranscribe()
 result=foo.transcribe_audio_file_path((foo.audio_file))
 print(result)
-
-# def text2int(textnum, numwords={}):
-#     if not numwords:
-#       units = [
-#         "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", 
-#         "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen",
-#         "sixteen", "seventeen", "eighteen", "nineteen",
-#       ]
-
-#       tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
-
-#       scales = ["hundred", "thousand", "million", "billion", "trillion"]
-
-#       numwords["and"] = (1, 0)
-#       for idx, word in enumerate(units):    numwords[word] = (1, idx)
-#       for idx, word in enumerate(tens):     numwords[word] = (1, idx * 10)
-#       for idx, word in enumerate(scales):   numwords[word] = (10 ** (idx * 3 or 2), 0)
-
-#     current = result = 0
-#     for word in textnum.split()
-
-#         scale, increment = numwords[word]
-#         current = current * scale + increment
-#         if scale > 100:
-#             result += current
-#             current = 0
-
-#     return result + current    
-
-# print(text2int(result))
-
-# Microsoft Bing Voice Recognition thinks you said ford junk vernon miles at eight zero zero bella rosa street suite three zero zero san antonio texas seven eight two zero seven your next individual hearing date is april thirteen two thousand seventeen at one PM before it jumps vernon miles at eight zero zero delarossa street suite three zero zero san antonio texas seven eight two zero seven william next hearing date press one for case processing information
