7ad1e77fcf86e79e69f3d992b35a587702cfc041
Leandra King <leandra.king@gmail.com>
2017-03-28 21:19:00 -0700

    added location_info_Google.py file, which gets location from transcripts that are parsed by Google (handled differerntly than transcripts parsed by Bing Speech recognition)

---
+++ extract/location_info_Google.py
@@ -0,0 +1,44 @@
+import re
+
+from uszipcode import ZipcodeSearchEngine
+
+import utils
+
+
+def get_re_for_location_parsing():
+    states_or = '|'.join(utils.states.keys())
+    return r"({states_or})".format(**locals()) + r" (\d{5})"
+
+def find_possible_locations(s):
+    q = get_re_for_location_parsing()
+    return list(set(re.findall(q, s)))
+
+def extract_location(s):
+    states = utils.states
+    z_search = ZipcodeSearchEngine()
+    possible_locations = find_possible_locations(s)
+    keys = ['State', 'City', 'Zipcode']
+    for state, zipcode in possible_locations:
+        zip_info = z_search.by_zipcode(zipcode)
+        if states[state] == zip_info['State']:
+            d = {key: zip_info[key] for key in keys}
+            d["Confidence"] = "high"
+            return d
+    print(possible_locations)
+    return {'State': possible_locations[0][0],
+            'City': None,
+            'Zipcode': possible_locations[0][1], 
+            'Confidence': "low"}
+    
+ 
+
+if __name__ == "__main__":
+    ss = [
+        'washington 98102',
+        'massachusetts 02138',
+        'texas 10983', # uszipcode fails as zip does not match state
+    ]
+    for s in ss:
+        print('\n' + s)
+        print(extract_location(s))
+    
